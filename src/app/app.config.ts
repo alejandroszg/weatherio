1008f9d16e9256eddface2830aa52f509a59661a
Alejandro Sánchez <alejandroszg@gmail.com>
2025-11-14 12:01:17 -0300

    chore: change PrimeNG version and imports
    styles: fixing dark and light colors according to system

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,7 @@ import { provideRouter } from '@angular/router';
 
 import { providePrimeNG } from 'primeng/config';
 import { MessageService } from 'primeng/api';
-import Aura from '@primeuix/themes/aura';
+import Aura from '@primeng/themes/aura';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -16,7 +16,7 @@ export const appConfig: ApplicationConfig = {
       theme: {
         preset: Aura,
         options: {
-          darkModeSelector: true || 'none',
+          darkModeSelector: 'system',
         },
       },
     }),
